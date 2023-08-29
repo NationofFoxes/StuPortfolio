8bae9125d94a23dd97d1d50d865075f7828de1d6
NationofFoxes <stuart.william.auld@gmail.com>
2023-08-29 14:42:45 +0900

    Moved to correct folder

---
+++ components/Navbar.vue
@@ -0,0 +1,49 @@
+<script>
+</script>
+
+<template>
+  <nav class="navbar">
+
+    <ul class="navbar-list">
+
+      <li class="navbar-item">
+        <NuxtLink to="/" class="navbar-link">
+          <button class="navbar-link  active" data-nav-link="about">About</button>
+        </NuxtLink>
+      </li>
+
+      <li class="navbar-item">
+        <NuxtLink to="/" class="navbar-link" >
+          <button class="navbar-link  active" data-nav-link="/resume.vue" >Resume</button>
+        </NuxtLink>
+      </li>
+
+      <li class="navbar-item">
+        <NuxtLink to="/" class="navbar-link">
+          <button class="navbar-link active" data-nav-link="/portfolio">Portfolio</button>
+        </NuxtLink>
+      </li>
+
+      <li class="navbar-item">
+        <NuxtLink to="/" class="navbar-link">
+          <button class="navbar-link  active" data-nav-link>Blog</button>
+        </NuxtLink>
+      </li>
+
+      <li class="navbar-item">
+        <NuxtLink to="/" class="navbar-link">
+          <button class="navbar-link  active" data-nav-link>Contact</button>
+        </NuxtLink>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped>
+a.router-link-active {
+  font-weight: bold;
+}
+a.router-link-exact-active {
+  color: var(--vegas-gold);
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<script setup>
+import About from "/components/About.vue"
+</script>
+
+
+<!-- maybe refer to about.vue? -->
+
+<template>
+  <article class="about active" data-page="about">
+    <About />
+  </article>
+</template>
+
+<style>
+@import 'assets\css\style.css';
+</style>
